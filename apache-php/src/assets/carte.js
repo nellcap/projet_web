f62a387a4873a330effedc7cf79eb83d3e858ceb
nellcap <nell.capelle@protonmail.com>
2025-11-16 17:47:06 +0100

    objet avec code

--- apache-php/src/assets/carte.js
+++ apache-php/src/assets/carte.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([48.8708852, 2.3170585], 15);
+var map = L.map('map').setView([48.22118448647317, -0.6951406610558623], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -12,6 +12,23 @@ elysee.bindPopup(message1).openPopup();
 var fermeviltain = L.marker([48.754300819108934, 2.1585445744449885]);
 var message2 = "Super, nous avons du lait. Il faut maintenant que nous fassions notre propre beurre.";
 
+var fromagerie = L.marker([48.22118448647317, -0.6951406610558623]);
+fromagerie.addTo(map);
+var message4 = "Super, nous avons du beurre."
+var message5 = "<div> <p>Nous avons besoin d'un code : </p><input type='text' id='reponse' placeholder='Votre texte...'><button onclick='verif_reponse(reponse)'>Valider</button></div>";
+fromagerie.bindPopup(message5).openPopup(); 
+function verif_reponse(reponse) {
+    var answer = reponse.value;
+    if (answer === '1312') {
+        fromagerie.bindPopup(message4).openPopup(); 
+    } else {
+        alert('Essayez encore...');
+    }
+}
+
+var message3 = "Nous avons besoin d'un code."
+
+
 var targetLat = 48.754300819108934;
 var targetLng = 2.1585445744449885;
 var tolerance = 0.005;
@@ -24,8 +41,10 @@ map.on('zoomend moveend', function () {
     var inTarget = Math.abs(center.lat - targetLat) <= tolerance &&
                    Math.abs(center.lng - targetLng) <= tolerance;
 
-    if (zoom >= 15 && inTarget) {
+    if (zoom >= 10 && inTarget) {
         fermeviltain.addTo(map);
         fermeviltain.bindPopup(message2).openPopup();
     }
 });
+
+
