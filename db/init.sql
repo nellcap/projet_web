94b3f164a5a6ea7ee313e50f69e4e2ff409a618c
claraxoxo <clara.bouvier@ensg.eu>
2025-11-19 22:47:28 +0100

    modification table objets

--- db/init.sql
+++ db/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS public.objets (
     position geometry(Point, 4326) NOT NULL,
     minZoomVisible INT DEFAULT 10,
     depart BOOLEAN DEFAULT FALSE,
-    typeObjet VARCHAR(50) NOT NULL CHECK (typeObjet IN ('debloquant', 'debloqué', 'code', 'obj_debloque_code', 'final')),
+    typeObjet VARCHAR(50) NOT NULL CHECK (typeObjet IN ('debloquant', 'debloque', 'code', 'obj_debloque_code', 'final')),
     code VARCHAR(4),
     messageDebut VARCHAR(250),
     messageFin VARCHAR(250),
@@ -22,7 +22,8 @@ CREATE TABLE IF NOT EXISTS public.objets (
 
 -- TRUNCATE objets;
 INSERT INTO objets (nom, position, minZoomVisible, depart, typeObjet, code, messageDebut, messageFin, url_image)
-VALUES ('lait',ST_GeomFromText('POINT(2.1585 48.7543)', 4326),9,TRUE,'debloquant',NULL,'quoicoubeh','Maintenant, allez voir le gagnant du prix agricole du beurre en 2024.','/data/lait.jpg');
+VALUES ('lait',ST_GeomFromText('POINT(2.1585 48.7543)', 4326),9,TRUE,'debloquant',NULL,'quoicoubeh','Maintenant, allez voir le gagnant du prix agricole du beurre en 2024.','/data/lait.jpg'),
+('beurre', ST_GeomFromText('POINT(-0.6951 48.2211)'),9, FALSE, 'debloque', NULL, 'Bravo ! Tu as trouvé le meilleur beurre agricole de 2024 !', 'Maintenant, va chercher des oeufs chez Garance.', '/data/beurre.png')
 
 
 -- objet_bloquant_id INT REFERENCES objets(id), -- Pour type 'bloque_objet'
